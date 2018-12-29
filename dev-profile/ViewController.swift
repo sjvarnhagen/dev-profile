5ea952a7e941295caf222e4d6f6a059e0060b348
Stanley Varnhagen <sjv1@ualberta.ca>
2018-12-28 20:43:34 -0700

    Initial Commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Stanley Varnhagen on 28-12-2018.
+//  Copyright © 2018 Stanley Varnhagen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
